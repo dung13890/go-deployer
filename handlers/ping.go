5b0d08c3b32c6ae3c758c897ab185a1bf933dd7d
Mr. D <dung13890@gmail.com>
2020-05-03 20:39:39 +0700

    refs #ping server
    refs #ping server

--- handlers/ping.go
+++ handlers/ping.go
@@ -1,5 +1,45 @@
 package handlers
 
-func ping() {
-	test()
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"sync"
+)
+
+type ping struct {
+	Client []client
+}
+
+func (p *ping) load() {
+
+}
+
+func (p *ping) run() {
+	c := Configuration{}
+	c.ReadFile()
+	pathKey := c.GetPathKey()
+	wg := sync.WaitGroup{}
+	i := 0
+	for k, s := range c.Hosts {
+		i++
+		wg.Add(1)
+		go func(w *sync.WaitGroup, s Server, k string, i int) {
+			defer w.Done()
+			out := bytes.Buffer{}
+			r := &remoteScript{}
+			r.Stdout = &out
+			if err := r.connection(s.Address, s.User, pathKey); err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			if err := r.run("uname -a"); err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			r.Color = clientColors[i%len(clientColors)]
+			fmt.Printf("%s:%s", fillColor(k, r.Color), fillColor(string(out.Bytes()), r.Color))
+			fmt.Printf("%s:%s\n", fillColor(k, r.Color), fillColor(":=====> OK", r.Color))
+		}(&wg, s, k, i)
+	}
+	wg.Wait()
+	return
 }

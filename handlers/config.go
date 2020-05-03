6d8c43ea57132f57af7c695aca6d66e983ad67a6
Mr. D <dung13890@gmail.com>
2020-05-03 20:39:39 +0700

    refs #ping server

--- handlers/config.go
+++ handlers/config.go
@@ -3,23 +3,27 @@ package handlers
 import (
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
 type Configuration struct {
-	WebServers
-}
-
-type WebServers struct {
-	Hosts map[string]Server `yaml:"hosts"`
-	Tasks []string          `yaml:"Tasks"`
+	Hosts   map[string]Server `yaml:"hosts"`
+	Tasks   []string          `yaml:"tasks"`
+	Setting Setting           `yaml:"setting"`
 }
 
 type Server struct {
 	Address string `yaml:"address"`
 	User    string `yaml:"user"`
 	Dir     string `yaml:"dir"`
+}
+
+type Setting struct {
+	PathKey string `yaml:"pathKey"`
 }
 
 func (c *Configuration) ReadFile() {
@@ -31,4 +35,20 @@ func (c *Configuration) ReadFile() {
 	if errY != nil {
 		log.Fatalf("error: %v", errY)
 	}
+}
+
+func (c *Configuration) GetPathKey() string {
+	path := c.Setting.PathKey
+	if path == "" {
+		return filepath.Join(os.Getenv("HOME"), ".ssh", "id_rsa")
+	}
+
+	replacePath := strings.Replace(path, "~", os.Getenv("HOME"), 1)
+	pathKey, err := filepath.Abs(replacePath)
+
+	if err != nil {
+		log.Fatal("Warning: path file of rsa key is not exists")
+	}
+
+	return pathKey
 }
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/dung13890/go-deployer/handlers"
 )
 
 func main() {
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		err = os.MkdirAll("logs", 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+	t := time.Now()
+	nameLog := fmt.Sprintf("./logs/deployer-%d-%02d-%02d.log", t.Year(), t.Month(), t.Day())
 	// Config Log path
-	file, err := os.OpenFile("./logs/deploy.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(nameLog, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
